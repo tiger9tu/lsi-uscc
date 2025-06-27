1ff596855d1fb22387c44db152b8eff18255b849
tiger9tu <tuy@uchicago.edu>
2025-06-27 17:12:05 -0500

    save

--- test.py
+++ test.py
@@ -38,7 +38,7 @@ def is_nn(idxes, config):
             print("Warning: belongs to no fragments, orb = ", orb)
     return len(fragments) <= 2 and config['adj'][fragments[0]][fragments[1]] == 1
 
-GLOBAL_MAX_CYCLE = 15000
+GLOBAL_MAX_CYCLE = 15000 # debug 
 
 
 class MolConfig(TypedDict):
@@ -87,12 +87,17 @@ def psi_kernel (fci, h1, h2, norb, nelec, norb_f=None, ci0_f=None,
             tol=1e-8, gtol=1e-6, max_cycle=None, 
             orbsym=None, wfnsym=None, ecore=0, opt = True, **kwargs):
     if norb_f is None: norb_f = getattr (fci, 'norb_f', [norb])
+    
     if ci0_f is None: ci0_f = fci.get_init_guess (norb, nelec, norb_f, h1, h2)
 
-    frozen = getattr (fci, 'frozen', None)
-    psi = getattr (fci, 'psi', fci.build_psi (ci0_f, norb, norb_f, nelec, frozen=frozen))
+    psi = getattr (fci, 'psi', fci.build_psi (ci0_f, norb, norb_f, nelec, frozen=None))
     assert (psi.check_ci0_constr)
-    
+
+    # debug
+    print("len(psi.x) = ", len(psi.x))
+    print("len(psi.uop.ngen_uniq) = ", psi.uop.ngen_uniq)
+    print("sum ([c.size for c in psi.ci_f] = ", sum ([c.size for c in psi.ci_f]))
+     
     if not opt:
         return psi 
     
@@ -174,6 +179,10 @@ def nci_test(a_idxs_selected, i_idxs_selected, config, mol, mc_uscc):
         for j in range(nc):
             S[i, j], H[i, j] = get_Sij_Hij(las_ucc_trial_cis[i], las_ucc_trial_cis[j], h)
 
+    print("S matrix:")
+    print(S)
+    print("H matrix:")
+    print(H)
     eigvals, eigvecs = eigh(H, S)
     return eigvals[0]  # Take the lowest eigenvalue as the energy
 
@@ -212,7 +221,8 @@ def test(mol_config, test_config,las, mc_uscc, mol):
         i_idxs_selected = i_idxs_selected_all
         
     mc_uscc.fcisolver = lasuccsd.FCISolver_USCC(mol, a_idxs_selected, i_idxs_selected)
-    # mc_uscc.fcisolver.norb_f = mol_config['ncas'] # number of orbitals in each fragment
+    mc_uscc.fcisolver.norb_f = mol_config['ncas'] # number of orbitals in each fragment
+    # easily hit the maximal memory limit
     mc_uscc.kernel()
     if not mc_uscc.converged:
         print('Warning: kernel hasn\'t converged')
@@ -260,167 +270,195 @@ def circle_adj(n):
     return [[1 if abs(i - j) == 1 or abs(i - j) == n-1 else 0 for j in range(n)] for i in range(n)]
 
 
+if __name__ == "__main__":
+    H6xyz = ''' H      0.000000000000   0.000000000000   0.000000000000
+    H      1.000000000000   0.000000000000   0.000000000000
+    H      0.273746762116   2.195450598147   0.100000000000
+    H      1.232912762116   1.895450598147  -0.100000000000
+    H      0.507178110854   4.193780995243   0.049334760036
+    H      1.506140937609   3.988021397347  -0.049334760036
+    '''
 
-H6xyz = ''' H      0.000000000000   0.000000000000   0.000000000000
-H      1.000000000000   0.000000000000   0.000000000000
-H      0.273746762116   2.195450598147   0.100000000000
-H      1.232912762116   1.895450598147  -0.100000000000
-H      0.507178110854   4.193780995243   0.049334760036
-H      1.506140937609   3.988021397347  -0.049334760036
-'''
+    data_dir = '/home/ndtm/repo/qchem/las_uccsd_data'
 
-h6_sto3g : MolConfig = {
-    'name': 'H6_STO3G',
-    'xyz': H6xyz,
-    'basis': 'sto-3g',
-    'ncas': [2, 2, 2],
-    'nelecas': [2, 2, 2],
-    'spinsub': [1, 1, 1],
-    'frag_atom_list': ((0, 1), (2, 3), (4, 5)),
-    'frag_spin_orb': {
-        0: (0, 1, 6, 7),
-        1: (2, 3, 8, 9),
-        2: (4, 5, 10, 11)
-    },
-    'adj': empty_adj(3),
-}
-
-h6_631g : MolConfig = {
-    'name': 'H6_631G',
-    'xyz': H6xyz,
-    'basis': '6-31g',
-    'ncas': [2, 2, 2],
-    'nelecas': [2, 2, 2],
-    'spinsub': [1, 1, 1],
-    'frag_atom_list': ((0, 1), (2, 3), (4, 5)),
-    'frag_spin_orb': {
-        0: (0, 1, 6, 7),
-        1: (2, 3, 8, 9),
-        2: (4, 5, 10, 11)
-    },
-    'adj': empty_adj(3),
-
-}
-
-data_dir = '/home/ndtm/repo/qchem/las_uccsd_data'
-
-with open(data_dir + '/stilbene/geometries/stil-90.xyz', 'r', encoding='utf-8') as f:
-    stil90xyz = f.read()
-
-stil_sto3g_90 : MolConfig = {
-    'name': 'STIL_STO3G_90',
-    'xyz': stil90xyz,
-    'basis': 'sto-3g',
-    'ncas': [4,2,4],
-    'nelecas': [4,2,4],
-    'spinsub': [1, 1, 1],
-    'frag_atom_list': [ [1,2,3,4,5,6,15,16,17,18,19] , [0,7, 14,20] , [8,9,10,11,12,13, 21,22,23,24,25] ],
-    'frag_spin_orb': {
-        0: (0, 1, 2, 3,10,11,12,13),
-        1: (4,5,14,15),
-        2: (6,7,8,9,16,17,18,19)
-    },
-    'adj': empty_adj(3),
-}
-
-with open(data_dir + '/polyenes/geometries/c10.xyz', 'r', encoding='utf-8') as f:
-    c10xyz = f.read()
-
-c10_sto3g : MolConfig = {
-    'name': 'C10_STO3G',
-    'xyz': c10xyz,
-    'basis': 'sto-3g',
-    'ncas': [2,2,2,2,2],
-    'nelecas': [2,2,2,2,2],
-    'spinsub': [1,1,1,1,1],
-    'frag_atom_list': [[0,2], [10,12], [18,19], [13,11], [3,1]],
-    'frag_spin_orb': {
-        0: (0,1,10,11),
-        1: (2,3,12,13),
-        2: (4,5,14,15),
-        3: (6,7,16,17),
-        4: (8,9,18,19)
-    },
-    'adj': circle_adj(5),
-}
-
-with open(data_dir + '/circle/H10.xyz', 'r', encoding='utf-8') as f:
-    h10_circle_xyz = f.read()
-
-h10_circle_sto3g : MolConfig = {
-    'name': 'H10_CIRCLE_STO3G',
-    'xyz': h10_circle_xyz,
-    'basis': 'sto-3g',
-    'ncas': [2,2,2,2,2],
-    'nelecas': [2,2,2,2,2],
-    'spinsub': [1,1,1,1,1],
-    'frag_atom_list': [[0,1], [2,3], [4,5], [6,7], [8,9]],
-    'frag_spin_orb': {
-        0: (0,1,10,11),
-        1: (2,3,12,13),
-        2: (4,5,14,15),
-        3: (6,7,16,17),
-        4: (8,9,18,19)
-    },
-    'adj': circle_adj(5),
-}
-
-mol_configs = [h6_sto3g, h6_631g, stil_sto3g_90, c10_sto3g, h10_circle_sto3g]
-
-noci_test_01 : TestConfig = {
-    'epsilon': 0.01,
-    'nn': False,
-    'max_nx': 10000,
-    'min_nc': 3,
-    'init_method': 'uscc_opt',
-    'grad_test': False,
-    'noci_test': True
-}
+    with open(data_dir + '/stilbene/geometries/stil-90.xyz', 'r', encoding='utf-8') as f:
+        stil90xyz = f.read()
     
-noci_test_001 = copy.deepcopy(noci_test_01)
-noci_test_001['epsilon'] = 0.001
+    with open(data_dir + '/polyenes/geometries/c4.xyz', 'r', encoding='utf-8') as f:
+        c4xyz = f.read()
 
-noci_test_0001 = copy.deepcopy(noci_test_01)
-noci_test_0001['epsilon'] = 0.0001
+    with open(data_dir + '/polyenes/geometries/c6.xyz', 'r', encoding='utf-8') as f:
+        c6xyz = f.read()
 
-tests = [
-    noci_test_01,
-    noci_test_001,
-    # noci_test_0001
-]
+    with open(data_dir + '/polyenes/geometries/c10.xyz', 'r', encoding='utf-8') as f:
+        c10xyz = f.read()
 
-
-for mol_conf in mol_configs:
-    print(f"Molecule {mol_conf['name']}: ")
-    mol_results, ref_energy, las_energy = batch_test(mol_conf, tests)
+    with open(data_dir + '/circle/H10.xyz', 'r', encoding='utf-8') as f:
+        h10_circle_xyz = f.read()
     
-    print(f"Reference energy: {ref_energy}")
-    print(f"MC-LAS energy: {las_energy}")
-    for result in mol_results:
-        print(f"Total excitations: {result['tot_excitation_count']}")
-        # print(f"NN excitations: {result['excitation_count_nn']}")
+    h6_sto3g : MolConfig = {
+        'name': 'H6_STO3G',
+        'xyz': H6xyz,
+        'basis': 'sto-3g',
+        'ncas': [2, 2, 2],
+        'nelecas': [2, 2, 2],
+        'spinsub': [1, 1, 1],
+        'frag_atom_list': ((0, 1), (2, 3), (4, 5)),
+        'frag_spin_orb': {
+            0: (0, 1, 6, 7),
+            1: (2, 3, 8, 9),
+            2: (4, 5, 10, 11)
+        },
+        'adj': empty_adj(3),
+    }
 
-        if 'las_uscc_eng' in result:
-            print(f"MC-USCC energy: {result['las_uscc_eng']:.12f}")
-        if 'las_uscc_noci_eng' in result:
-            print(f"MC-USCC-NOCI energy: {result['las_uscc_noci_eng']:.12f}")
-        if 'tot_g' in result:
-            print(f"Total gradient norm: {np.linalg.norm(result['tot_g']):.12f}")
-        if 'nn_g' in result:
-            print(f"NN gradient norm: {np.linalg.norm(result['nn_g']):.12f}")
-        print("\n")
-    
-    print("\n\n")
+    h6_631g : MolConfig = {
+        'name': 'H6_631G',
+        'xyz': H6xyz,
+        'basis': '6-31g',
+        'ncas': [2, 2, 2],
+        'nelecas': [2, 2, 2],
+        'spinsub': [1, 1, 1],
+        'frag_atom_list': ((0, 1), (2, 3), (4, 5)),
+        'frag_spin_orb': {
+            0: (0, 1, 6, 7),
+            1: (2, 3, 8, 9),
+            2: (4, 5, 10, 11)
+        },
+        'adj': empty_adj(3),
+
+    }
+
+    stil_sto3g_90 : MolConfig = {
+        'name': 'STIL_STO3G_90',
+        'xyz': stil90xyz,
+        'basis': 'sto-3g',
+        'ncas': [4,2,4],
+        'nelecas': [4,2,4],
+        'spinsub': [1, 1, 1],
+        'frag_atom_list': [ [1,2,3,4,5,6,15,16,17,18,19] , [0,7, 14,20] , [8,9,10,11,12,13, 21,22,23,24,25] ],
+        'frag_spin_orb': {
+            0: (0, 1, 2, 3,10,11,12,13),
+            1: (4,5,14,15),
+            2: (6,7,8,9,16,17,18,19)
+        },
+        'adj': empty_adj(3),
+    }
+
+    # This c4 is too small, and I observed numericall error, that H and S have all identical
+    # entries however the NOCI energy is lower 
+    c4_sto3g : MolConfig = {
+        'name': 'C4_STO3G',
+        'xyz': c4xyz,
+        'basis': 'sto-3g',
+        'ncas': [2,2],
+        'nelecas': [2,2],
+        'spinsub': [1,1],
+        'frag_atom_list':  [[0,2], [3,1]],
+        'frag_spin_orb': {
+            0: (0,1,4,5),
+            1: (2,3,6,7),
+        },
+        'adj': circle_adj(2), # not sure about this
+    }
+
+    # c6_sto3g : MolConfig = {
+    #     'name': 'C6_STO3G',
+    #     'xyz': c6xyz,
+    #     'basis': 'sto-3g',
+    #     'ncas': [2,2,2],
+    #     'nelecas': [2,2,2],
+    #     'spinsub': [1,1,1],
+    #     # 'frag_atom_list': [[0,2], [10,12], [18,19], [13,11], [3,1]], ?? unknown
+    #     'frag_spin_orb': {
+    #         0: (0,1,6,7),
+    #         1: (2,3,8,9),
+    #         2: (4,5,10,11),
+    #     },
+    #     'adj': circle_adj(3),
+    # }
+
+    c10_sto3g : MolConfig = {
+        'name': 'C10_STO3G',
+        'xyz': c10xyz,
+        'basis': 'sto-3g',
+        'ncas': [2,2,2,2,2],
+        'nelecas': [2,2,2,2,2],
+        'spinsub': [1,1,1,1,1],
+        'frag_atom_list': [[0,2], [10,12], [18,19], [13,11], [3,1]],
+        'frag_spin_orb': {
+            0: (0,1,10,11),
+            1: (2,3,12,13),
+            2: (4,5,14,15),
+            3: (6,7,16,17),
+            4: (8,9,18,19)
+        },
+        'adj': circle_adj(5),
+    }
 
 
+    h10_circle_sto3g : MolConfig = {
+        'name': 'H10_CIRCLE_STO3G',
+        'xyz': h10_circle_xyz,
+        'basis': 'sto-3g',
+        'ncas': [2,2,2,2,2],
+        'nelecas': [2,2,2,2,2],
+        'spinsub': [1,1,1,1,1],
+        'frag_atom_list': [[0,1], [2,3], [4,5], [6,7], [8,9]],
+        'frag_spin_orb': {
+            0: (0,1,10,11),
+            1: (2,3,12,13),
+            2: (4,5,14,15),
+            3: (6,7,16,17),
+            4: (8,9,18,19)
+        },
+        'adj': circle_adj(5),
+    }
+
+    mol_configs = [c4_sto3g, h10_circle_sto3g, h6_sto3g, h6_631g,stil_sto3g_90, c10_sto3g ]
+
+    noci_test_01 : TestConfig = {
+        'epsilon': 0.01,
+        'nn': False,
+        'max_nx': 10000,
+        'min_nc': 3,
+        'init_method': 'uscc_opt',
+        'grad_test': False,
+        'noci_test': True
+    }
+        
+    noci_test_001 = copy.deepcopy(noci_test_01)
+    noci_test_001['epsilon'] = 0.001
+
+    noci_test_0001 = copy.deepcopy(noci_test_01)
+    noci_test_0001['epsilon'] = 0.0001
+
+    tests = [
+        noci_test_01,
+        noci_test_001,
+        # noci_test_0001
+    ]
 
 
+    for mol_conf in mol_configs:
+        print(f"Molecule {mol_conf['name']}: ")
+        mol_results, ref_energy, las_energy = batch_test(mol_conf, tests)
+        
+        print(f"Reference energy: {ref_energy}")
+        print(f"MC-LAS energy: {las_energy}")
+        for result in mol_results:
+            print(f"Total excitations: {result['tot_excitation_count']}")
+            # print(f"NN excitations: {result['excitation_count_nn']}")
 
-
-
-
-
-
-
-
+            if 'las_uscc_eng' in result:
+                print(f"MC-USCC energy: {result['las_uscc_eng']:.12f}")
+            if 'las_uscc_noci_eng' in result:
+                print(f"MC-USCC-NOCI energy: {result['las_uscc_noci_eng']:.12f}")
+            if 'tot_g' in result:
+                print(f"Total gradient norm: {np.linalg.norm(result['tot_g']):.12f}")
+            if 'nn_g' in result:
+                print(f"NN gradient norm: {np.linalg.norm(result['nn_g']):.12f}")
+            print("\n")
+        
+        print("\n\n")
 
